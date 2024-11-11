32c074d21545ded01d6941a0f3eb35bdce2f2b32
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2024-11-11 12:48:42 +0400

    Add files via upload

---
+++ CheckVowelOrNotVowelNumber.cpp
@@ -0,0 +1,44 @@
+//
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+char ReadLetter()
+{
+
+    char letter;
+
+    cout <<"Enter The Word: ";
+    cin >> letter;
+    
+    return letter;
+}
+
+
+bool IsVowel(char letter)
+{
+    letter = tolower(letter);
+
+    return((letter == 'a') || (letter == 'e') || (letter == 'i') || (letter == 'o')|| (letter == 'u'));
+}
+
+
+int main()
+{
+
+
+    char letter = ReadLetter();
+
+    if(IsVowel(letter))
+    
+        cout <<"\nYes letter " << letter << " is vowel";
+    
+    else
+        cout <<"\nNo letter " << letter << " is not vowel";
+
+        return 0;
+
+}
