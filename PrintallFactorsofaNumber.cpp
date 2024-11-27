f286a2ade00d8cb7874fc4c47c42906dbb7df1bd
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2024-11-27 13:12:32 +0400

    Update PrintallFactorsofaNumber.cpp

--- PrintallFactorsofaNumber.cpp
+++ PrintallFactorsofaNumber.cpp
@@ -2,25 +2,43 @@
 Print all Factors of a Number:
 Create a program to find the factors of a number.
 */
+
 #include <iostream>
+
 using namespace std;
 
-int main() {
 
-    int number;
-    cin >> number;
+int ReadIntger()
+{
+    int Number;
 
-    // loop from 1 to number
-    for (int i = 1; i <= number; i++) {
+    cout << "Enter The Number: ";
+    cin >> Number;
 
-        // check if number is divisible by i
 
-        if(number % i == 0)
+    return Number;
+}
+
+
+void PrintallFactorsofaNumber(int Number)
+{
+    for(int i = 1; i <= Number; i++)
+    {
+        if(Number % i == 0)
         {
             cout << i << endl;
         }
-
     }
+}
+
+
+int main()
+{
+
+    int Number =  ReadIntger();
+
+    PrintallFactorsofaNumber(Number);
 
     return 0;
+
 }
